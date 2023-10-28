4e3be8792bb33fac0709fa09b23016bdf42e11af
20102181buigiadai <20102181.dai@student.iuh.edu.vn>
2023-10-28 21:54:08 +0530

    Lấy được vị trí bản thân, xử lý nút định vị, thêm vị trí cửa hàng(NearbyStore.js)

--- QuaTangVip/src/screeens/NearbyStore.js
+++ QuaTangVip/src/screeens/NearbyStore.js
@@ -1,69 +1,108 @@
-// Import các thư viện cần thiết
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+import React, { useEffect, useState, useRef } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { useNavigation } from "@react-navigation/native";
+import { Icon } from "react-native-elements";
 
-const MapScreen = () => {
+const MapScreen = ({ navigation, route }) => {
+  const goBackImage = require("../../assets/icons_Dai/node_modules_reactnavigation_stack_src_views_assets_backicon.png");
+  const myLocationImage = require("../../assets/icons_Dai/ic_my_location.webp");
+  const colorYellow = "#FFC62E";
+
+  const button = [
+    {
+      id: 1,
+      name: "Trở về",
+      icon: goBackImage,
+      backgroundColor: colorYellow,
+      color: "black",
+      event: handleGoBack
+    },
+    {
+      id: 2,
+      name: "Định vị",
+      icon: myLocationImage,
+      backgroundColor: colorYellow,
+      color: "black",
+      event: handleLocateMe
+    }
+  ];
+
   const [location, setLocation] = useState(null);
+  const mapViewRef = useRef(null);
   const [stores, setStores] = useState([
-    { id: 1, name: 'Store 1', latitude: 37.7749, longitude: -122.4194 },
-    { id: 2, name: 'Store 2', latitude: 37.7899, longitude: -122.4354 },
-    // Thêm các cửa hàng khác vào đây
+    { id: 1, name: "Store 1", latitude: 10.82213, longitude: 106.68683 },
+    { id: 2, name: "Store 2", latitude: 10.82418, longitude: 106.68677 },
+    { id: 3, name: "Store 3", latitude: 10.82313, longitude: 106.68783 }
   ]);
 
   useEffect(() => {
-    // Hàm lấy vị trí hiện tại
     const getLocation = async () => {
       try {
-        // Yêu cầu quyền truy cập vị trí
         let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-          console.error('Permission to access location was denied');
+        if (status !== "granted") {
+          console.error("Quyền truy cập vị trí đã bị từ chối");
           return;
         }
 
-        // Lấy thông tin vị trí
         let userLocation = await Location.getCurrentPositionAsync({});
         setLocation(userLocation.coords);
       } catch (error) {
-        console.error('An error occurred while getting the location', error);
+        console.error("Đã xảy ra lỗi khi nhận vị trí", error);
       }
     };
-
-    // Gọi hàm lấy vị trí khi màn hình được load
     getLocation();
-  }, []); // Chạy chỉ một lần khi màn hình được load
+  }, []);
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  const handleLocateMe = async () => {
+    try {
+      let userLocation = await Location.getCurrentPositionAsync({});
+      setLocation(userLocation.coords);
+
+      mapViewRef.current?.animateToRegion({
+        latitude: userLocation.coords.latitude,
+        longitude: userLocation.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      });
+    } catch (error) {
+      console.error("Đã xảy ra lỗi khi nhận vị trí", error);
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
       {location ? (
         <MapView
+          ref={mapViewRef}
           style={{ flex: 1 }}
           initialRegion={{
             latitude: location.latitude,
             longitude: location.longitude,
             latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            longitudeDelta: 0.0421
           }}
         >
-          {/* Đánh dấu vị trí hiện tại */}
           <Marker
             coordinate={{
               latitude: location.latitude,
-              longitude: location.longitude,
+              longitude: location.longitude
             }}
             title="You are here"
             description="Your current location"
           />
 
-          {/* Đánh dấu các cửa hàng từ dữ liệu */}
-          {stores.map(store => (
+          {stores.map((store) => (
             <Marker
               key={store.id}
               coordinate={{
                 latitude: store.latitude,
-                longitude: store.longitude,
+                longitude: store.longitude
               }}
               title={store.name}
               description={`Location of ${store.name}`}
@@ -73,6 +112,49 @@ const MapScreen = () => {
       ) : (
         <Text>Loading...</Text>
       )}
+
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          marginTop: 80,
+          padding: 10,
+          zIndex: 1,
+          flexDirection: "row",
+          justifyContent: "space-between"
+        }}
+      >
+        <TouchableOpacity
+          onPress={handleGoBack}
+          style={{
+            backgroundColor: colorYellow,
+            borderRadius: 25,
+            width: 40,
+            height: 40,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Image source={goBackImage} style={{ width: 30, height: 30 }} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleLocateMe}
+          style={{
+            backgroundColor: colorYellow,
+            width: 40,
+            height: 40,
+            borderColor: colorYellow,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Image
+            source={myLocationImage}
+            style={{ width: 30, height: 30, tintColor: "black" }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
